01aeb0148c55a75535dc748b7d0135b6daad5013
James Merriman <mezzarino@outlook.com>
2023-06-20 16:32:59 +0100

    test adding cypress and percy to test automated UI

---
+++ components/widget.cy.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Widget from './widget'
+
+describe('<Widget />', () => {
+  it('Widget renders correctly', () => {
+    cy.mount(<Widget />);
+  });
+
+  it('Widget has an h2 present', () => {
+    cy.mount(<Widget />);
+    cy.get('h2').should('be.visible');
+
+    cy.percySnapshot('Widget component');
+  });
+});
